416fbf42b2b35d162260da1bf541927a912256d3
Lei ZHU <lzhu19@slb.com>
2020-11-30 15:11:50 +0800

    feat:can generate group of counter components

---
+++ src/app/counter-group/counter-group.component.spec.ts
@@ -0,0 +1,34 @@
+import { Counter } from './../models/counter';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterGroupComponent } from './counter-group.component';
+
+describe('CounterGroupComponent', () => {
+  let component: CounterGroupComponent;
+  let fixture: ComponentFixture<CounterGroupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CounterGroupComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterGroupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set size when create instance', () => {
+    expect(component.size).toBe(5);
+  });
+
+  it('should contain counters when create instance', () => {
+    expect(component.counters.length).toBe(component.size);
+  });
+});
---
+++ src/app/counter-group/counter-group.component.ts
@@ -0,0 +1,25 @@
+import { Counter } from './../models/counter';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-group',
+  templateUrl: './counter-group.component.html',
+  styleUrls: ['./counter-group.component.css']
+})
+export class CounterGroupComponent implements OnInit {
+
+  constructor() {
+    this.counters = new Array<Counter>();
+  }
+  public size: number = 5;
+
+  public counters: Array<Counter>;
+
+  ngOnInit(): void {
+    for (let step = 0; step < this.size; step++){
+      this.counters.push(new Counter());
+    }
+  }
+
+
+}
