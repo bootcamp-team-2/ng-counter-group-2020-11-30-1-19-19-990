1b91633986e067c8c179ada3cf68c4ba90e8ef8e
Lin LIU <lliu41@slb.com>
2020-11-30 15:12:07 +0800

    feat: can generate group of counter components

--- src/app/counter-group/counter-group.component.spec.ts
+++ src/app/counter-group/counter-group.component.spec.ts
@@ -25,5 +25,9 @@ describe('CounterGroupComponent', () => {
 
   it('should set size when create instance', () => {
     expect(component.size).toBe(5);
-  })
+  });
+
+  it('should contain counters when create instance', () => {
+    expect(component.counters.length).toBe(component.size);
+  });
 });
