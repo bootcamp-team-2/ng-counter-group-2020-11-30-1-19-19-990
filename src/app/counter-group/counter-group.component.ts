0d75ce020933b8477e2d6869682013eb42a319fa
YYuan1414 <yuanyiren19900728@163.com>
2020-11-30 14:55:41 +0800

    feat: add size into counterGroup

---
+++ src/app/counter-group/counter-group.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-group',
+  templateUrl: './counter-group.component.html',
+  styleUrls: ['./counter-group.component.css']
+})
+export class CounterGroupComponent implements OnInit {
+
+  constructor() { }
+
+  public size: number = 5;
+  ngOnInit(): void {
+  }
+
+}
